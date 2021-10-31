ec2d7bdf19f6a8f627a133c9490a78f398594823
zexianli <zexianlie@gmail.com>
2021-10-30 23:35:38 -0600

    Started graphql

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8000;
+const express = require('express');
+const morgan = require('morgan');
+const api = require('./api');
+const { connectToDB } = require('./lib/mongo');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const resolvers = {
+  Query: {
+    hello: () => 'hello',
+  },
+};
+
+// app.use('/', api);
+
+// app.use('*', (req, res, next) => {
+//   res.send('Hello World!');
+// });
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const port = process.env.PORT || 8000;
+
+  app.use(express.json());
+  app.use(morgan('dev'));
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+  connectToDB(async () => {
+    app.listen(port, () => {
+      console.log(
+        `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+      );
+    });
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
